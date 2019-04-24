d6fac5a3a42c95e0564ca33bcce105c55c2d1166
RoryLetteney <rory.letteney@gmail.com>
2019-04-24 09:04:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Project Name
+
+**Author**: Rory Letteney
+**Version**: 1.0.6
+
+## Overview
+This application takes in user input in the form of a city name, and returns the weather for said location.
+
+## Getting Started
+To use this app:
+> npm install
+
+## Architecture
+We are using JavaScript, NodeJS, Express, and CORS. Using NodeJS we send a request to the Dark Sky API to retrieve the weather data for a user's query.
+
+## Change Log
+01-01-2001 4:59pm - Application now has a fully-functional express server, with GET routes for the location resource and weather resource. Also handles status 500 errors.
+
+## Credits and Collaborations
+Michele Saba - https://github.com/MicheleSaba
+David
+
+
+Number and name of feature: Feature #1: Location Route
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 9:36am
+
+Finish time: 10:15am
+
+Actual time needed to complete: 39 minutes
+
+Number and name of feature: Feature #2: Weather Route
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 10:15am
+
+Finish time: 10:48am
+
+Actual time needed to complete: 33 minutes
+
+Number and name of feature: Feature #3: Status 500 Error Handling
+
+Estimate of time needed to complete: 10 minutes
+
+Start time: 11:05am
+
+Finish time: 11:15am
+
+Actual time needed to complete: 10 minutes
+
+Number and name of feature: Feature #3:
+
+Estimate of time needed to complete: 
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete: 
+
+Number and name of feature: Feature #3:
+
+Estimate of time needed to complete: 
+
+Start time: 
+
+Finish time: 
+
+Actual time needed to complete: 
---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 3000;
+
+// CREATE LOCATION ROUTE
+app.get('/location', (req, res) => {
+  try {
+    // STORE THE USER'S QUERY-TURNED-LOCATION-OBJECT IN LOCATIONDATA
+    const locationData = searchToLatLong(req.query.data);
+    // RETURN THE LOCATION OBJECT
+    res.send(locationData);
+  } catch(err) {
+    errorHandler(res, 500, 'Please enter a location!');
+  }
+});
+
+// CREATE WEATHER ROUTE
+app.get('/weather', (req, res) => {
+  try {
+    // STORE THE USER'S QUERY LOCATION
+    const weatherData = getWeather();
+    res.send(weatherData);
+  } catch(err) {
+    errorHandler(res, 500, 'Please enter a location!');
+  }
+});
+
+// CREATE A NEW LOCATION OBJECT FOR THE USER'S QUERY
+const searchToLatLong = query => {
+  const geoData = require('./data/geo.json');
+  const location = new Location(query, geoData);
+  return location;
+};
+
+function Location(query, res) {
+  this.query = query,
+  this.formatted_query = res.results[0].formatted_address,
+  this.latitude = res.results[0].geometry.location.lat,
+  this.longitude = res.results[0].geometry.location.lng;
+}
+
+// RETURN ALL WEATHER RECORDS FOR THE USER'S LOCATION QUERY
+const getWeather = () => {
+  const darkskyData = require('./data/darksky.json');
+  const weatherSummaries = [];
+
+  darkskyData.daily.data.forEach(day => {
+    weatherSummaries.push(new Weather(day));
+  });
+
+  return weatherSummaries;
+};
+
+function Weather(day) {
+  this.forecast = day.summary,
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+app.listen(PORT, () => console.log(`App is up and running on ${PORT}`));
+
+const errorHandler = (res, status, message) => res.send({ status, message });
